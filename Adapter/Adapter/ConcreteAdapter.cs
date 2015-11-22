e15024e35be5375c6b5fa1fec9b374de536a52bd
sawafrolov <sawa.frolov@yandex.ru>
2015-11-22 06:46:27 +0300

    Adapter

---
+++ Adapter/Adapter/ConcreteAdapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter.Adapter
+{
+    class ConcreteAdapter : ADapter
+    {
+        private Adaptee adaptee;
+
+        public ConcreteAdapter()
+        {
+            adaptee = new Adaptee();
+        }
+
+        public void Operation()
+        {
+            adaptee.AdapteeOperation();
+        }
+    }
+}
---
+++ Adapter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Adapter.Adapter;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ADapter adapter = new ConcreteAdapter();
+            adapter.Operation();
+            Console.ReadKey();
+        }
+    }
+}

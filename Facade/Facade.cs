a4e637b793374d2fae2c1678e700885e7747216b
ValentinaZorina <vvzorina95@gmail.com>
2015-12-15 20:06:31 +0300

    Facade pattern

---
+++ Facade/Facade.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FacadePattern
+{
+    class Facade
+    {
+        public void Method(){
+
+            Service service = new Service();
+
+            service.methodA();
+
+            service.methodB();
+
+            service.methodC();
+            
+        }
+    }
+}
---
+++ Facade/Service.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FacadePattern
+{
+    class Service
+    {
+        public void methodA()
+        {
+            Console.WriteLine("вызван метод А");
+        }
+
+        public void methodB()
+        {
+            Console.WriteLine("вызван метод B");
+        }
+
+        public void methodC()
+        {
+            Console.WriteLine("вызван метод C");
+        }
+    }
+}

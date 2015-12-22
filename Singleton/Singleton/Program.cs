185d99d934a0593b0953c304d1edd4c396f49b29
YaJou <hettter@gmail.com>
2015-12-22 18:31:34 +0300

    Singletonsss

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main()
+        {
+            Singletonclass MyInstance = Singletonclass.Sample();
+            Singletonclass MyInstance2 = Singletonclass.Sample();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Singleton/Singleton/Singletonclass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Singleton
+{
+    public class Singletonclass
+    {
+        private static readonly object obj = new object();
+        private static Singletonclass example;
+
+        private Singletonclass()
+        {
+            Console.WriteLine("Instance is created");
+        }
+
+        public static Singletonclass Sample()
+        {
+            if (example == null) 
+            {
+                lock (obj)
+                {
+                    if (example == null)
+                    {
+                        example = new Singletonclass();
+                    }
+                }
+            }
+            return example;
+        }
+    }
+}

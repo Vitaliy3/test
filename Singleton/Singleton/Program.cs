f8a516d208bf00682c7d91f13c193bc9905f90da
YaJou <hettter@gmail.com>
2015-12-22 22:01:16 +0300

    NewSigleton

--- Singleton/Singleton/Program.cs
+++ Singleton/Singleton/Program.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Singleton
+namespace SingletonPat
 {
     class Program
     {
         static void Main()
         {
-            Singletonclass MyInstance = Singletonclass.Sample();
-            Singletonclass MyInstance2 = Singletonclass.Sample();
+            Singleton MyInstance = Singleton.Sample();
+            Singleton MyInstance2 = Singleton.Sample();
 
             Console.ReadKey();
 
---
+++ Singleton/Singleton/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SingletonPat
+{
+    public class Singleton
+    {
+        // mutex защита для использования нескольких потоков одновременно
+        private static readonly object mutex = new object();
+        private static Singleton example;
+
+        private Singleton()
+        {
+            Console.WriteLine("Instance is created");
+        }
+
+        public static Singleton Sample()
+        {// Если example null, то создаем его
+            if (example == null) 
+            {//пока один поток (первый instance имеет mutex) все остальные потоки будут блокированы
+                lock (mutex)
+                {
+                    if (example == null)
+                    {// первый поток может создать наш Singleton
+                        example = new Singleton();
+                    }
+                }
+            }
+            return example;
+        }
+    }
+}

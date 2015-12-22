799baf6033db27c362f79236ab49a57a658148b4
YaJou <hettter@gmail.com>
2015-12-23 00:04:24 +0300

    Patter №3 Strategy

---
+++ StrategyPattern/StrategyPattern/Character.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StrategyPattern
+{
+    public class Character
+    {
+        private iWeapon weapon;
+
+        public void SetWeapon(iWeapon newWeapon)
+        {
+            weapon = newWeapon;
+        }
+
+        public void Attack()
+        {
+            weapon.Use();
+        }
+    }
+}
---
+++ StrategyPattern/StrategyPattern/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StrategyPattern
+{
+    class Program
+    {
+        static void Main()
+        {
+            iWeapon weapon = null;
+            Character YaJou = new Character();
+
+            while (true)
+            {
+                Console.WriteLine("Choose a weapon for me. 1 - Sword, 2 - Axe, 3 - Stone");
+                String input = Console.ReadLine();
+
+                switch(input)
+                {
+                    case "1":
+                        weapon = new Sword(); break;
+                    case "2":
+                        weapon = new Axe(); break;
+                    case "3":
+                        weapon = new Stone(); break;
+                }
+
+                YaJou.SetWeapon(weapon);
+                YaJou.Attack();
+            }
+        }
+    }
+}

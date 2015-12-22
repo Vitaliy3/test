204ceb1c639c082da128279ee3f10b862c7b36be
Nastakin <AkiAsy@yandex.ru>
2015-12-22 21:03:48 +0300

    #Nastya_branch. Facade and Prototype patterns has been added.

---
+++ Fasade/Fasade/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fasade
+{
+    internal class SubsystemA
+    {
+        internal string A1()
+        {
+            return "Subsystem A, Method A1\n";
+        }
+        internal string A2()
+        {
+            return "Subsystem A, Method A2\n";
+        }
+    }
+
+    internal class SubsystemB
+    {
+        internal string B1()
+        {
+            return "Subsystem B, Method B1\n";
+        }
+    }
+
+    internal class SubsystemC
+    {
+        internal string C1()
+        {
+            return "Subsystem C, Method C1\n";
+        }
+    }
+
+    public static class Facade
+    {
+        static Fasade.SubsystemA a = new Fasade.SubsystemA();
+        static Fasade.SubsystemB b = new Fasade.SubsystemB();
+        static Fasade.SubsystemC c = new Fasade.SubsystemC();
+
+        public static void Operation1()
+        {
+            Console.WriteLine("Operation 1\n" + a.A1() + a.A2() + b.B1());
+        }
+
+        public static void Operation2()
+        {
+            Console.WriteLine("Operation 2\n" + b.B1() + c.C1());
+        }
+    }
+
+    class Program
+    {              
+        static void Main(string[] args)
+        {
+            Facade.Operation1();
+            Facade.Operation2();
+            Console.Read();
+        }
+    }
+}
---
+++ Prototype/Prototype/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Prototype
+{
+    class MainApp
+    {
+        static void Main()
+        {
+            //Создание 2-х экземпляров и клонировние
+            Prototype prototype1 = new ConcretePrototype1("1");
+            Prototype clonedPrototype1 = prototype1.Clone();
+            Console.WriteLine("Cloned: {0}", clonedPrototype1.Id);
+
+            Prototype prototype2 = new ConcretePrototype2("2");
+            Prototype clonedPrototype2 = prototype2.Clone();
+            Console.WriteLine("Cloned: {0}", clonedPrototype2.Id);
+
+            Console.ReadKey();
+        }
+    }
+
+    public abstract class Prototype
+    {
+        public Prototype(string id)
+        {
+            this.Id = id;
+
+            Console.Write("Base constructor is called.");
+        }
+
+        public string Id { get; set; }
+
+        public virtual Prototype Clone()
+        {
+            return (Prototype)this.MemberwiseClone();
+        }
+    }
+
+    public class ConcretePrototype1 : Prototype
+    {
+        public ConcretePrototype1(string id)
+            : base(id)
+        {
+        }
+    }
+
+    public class ConcretePrototype2 : Prototype
+    {
+        public ConcretePrototype2(string id)
+            : base(id)
+        {
+        }
+    }
+}
